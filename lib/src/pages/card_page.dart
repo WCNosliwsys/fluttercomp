5db58c3eeeb25ccce40aecbb924a3898fe51d481
Wilson Callisaya <demarktec@gmail.com>
2019-11-22 18:27:17 -0500

    Estilo de tarjetas dos tipos de bordes redondeados

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -10,13 +10,32 @@ class CardPage extends StatelessWidget {
         children: <Widget>[
           _cardTipo1(),
           SizedBox(height: 30.0),
-          _cardTipo2()
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
         ],
       ),
     );
   }
   Widget _cardTipo1(){
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -43,7 +62,8 @@ class CardPage extends StatelessWidget {
   }
 
   Widget _cardTipo2(){
-    return Card(
+    final card= Container(
+
       child: Column(
         children: <Widget>[
           FadeInImage(
@@ -60,6 +80,25 @@ class CardPage extends StatelessWidget {
             padding: EdgeInsets.all(10.0),
               child: Text("No tengo idea que poner"))
         ],
+      ),
+    );
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color:Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0,10.0)
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
+
       ),
     );
   }
